bca71a0d1880e84e1799ddf8bc29d2d56f5d18f6
Ishita2499 <100797329+Ishita2499@users.noreply.github.com>
2022-08-22 01:55:19 -0400

    test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
 
 console.log("Your index.js file is loaded correctly!");
 <script>
-$(document).ready(function(){
-  $("Hero").click(function(){
-    $("#image2int").fadeIn();
-    $("#image2int").fadeout(1000);
-  });
-});
+	function bounce(bouncingCount, speed) {
+		var top = 200;
+		var speedRatio = speed / top;
+		var heightRatio = top / bouncingCount;
+
+		for (var i = 1; i <= bouncingCount; i++) {
+			$('#image2').animate({
+				'bottom' : top
+			}, speed);
+			$('#image2').animate({
+				'bottom' : 0
+			}, speed / 2);
+			top = top - (heightRatio);
+			speed = speedRatio * top;
+		}
+	}
+
+	$("#image2").click(function() {
+		bounce(10, 500);
+	});
 </script>
   
