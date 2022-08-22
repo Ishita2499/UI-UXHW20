e742b80e8f75e99d542f81bc31d9256394ac4285
Ishita2499 <100797329+Ishita2499@users.noreply.github.com>
2022-08-22 01:26:46 -0400

    Comit

--- js/index.js
+++ js/index.js
@@ -1,25 +1,11 @@
 
 console.log("Your index.js file is loaded correctly!");
-// Show active menu when scrolling
-const menu = document.querySelector('#desktop-menu');
-const menuLinks = document.querySelector('.nav__menu');
-
-// Display  Menu
-const desktopMenu = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-  };
-  
-  menu.addEventListener('click', desktopMenu);
-  
-  // Show active menu when scrolling
-  const highlightMenu = () => {
-    const elem = document.querySelector('.highlight');
-    const aboutMenu = document.querySelector('#about-page');
-    const projectsMenu = document.querySelector('#projects-page');
-    const resumeMenu = document.querySelector('#resume-page');
-    let scrollPos = window.scrollY;
-    // console.log(scrollPos);
-  }
-  
+<script>
+$(document).ready(function(){
+  $("Hero").click(function(){
+    $("#image2int").fadeIn();
+    $("#image2int").fadeout(1000);
+  });
+});
+</script>
   
