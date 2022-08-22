7c68f944bddc5b62432786e521cdbb07dd7e4d16
Ishita2499 <100797329+Ishita2499@users.noreply.github.com>
2022-08-22 02:49:04 -0400

    SOS

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
 
 console.log("Your index.js file is loaded correctly!");
-<script>
-	function bounce(bouncingCount, speed) {
-		var top = 200;
-		var speedRatio = speed / top;
-		var heightRatio = top / bouncingCount;
 
-		for (var i = 1; i <= bouncingCount; i++) {
-			$('#image2').animate({
-				'bottom' : top
-			}, speed);
-			$('#image2').animate({
-				'bottom' : 0
-			}, speed / 2);
-			top = top - (heightRatio);
-			speed = speedRatio * top;
-		}
-	}
+//Skills
 
-	$("#image2").click(function() {
-		bounce(10, 500);
-	});
-</script>
-  
+$("#WebDesign").hover(function(){
+    $("#web-design-2").fadeToggle();
+  });
+
+  $("#BrandIdentity").hover(function(){
+    $("#brands").fadeToggle();
+  });
+
+  $("#UI/UX").hover(function(){
+    $("#UX").fadeToggle();
+  });
+
+  $("#HTML").hover(function(){
+    $("#code").fadeToggle();
+  });
+
+  $("#Figma").hover(function(){
+    $("#figma").fadeToggle();
+});
+
+  $("#Miro").hover(function(){
+    $("#miro").fadeToggle();
+});
+ 
+
